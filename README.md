6724f5a180a445c02326d6f3f5c7a73145091ae2
cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-04-07 21:28:51 +0100

    :sparkles: added tomato bullet

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ A simple 3D game made with rust using ECS pattern with Bevy
 
 It uses mold as dynamic linker
 
-Ferris 3D model credit: https://github.com/RayMarch/ferris3d
+(Asset credit: Ferris 3D model: https://github.com/RayMarch/ferris3d)
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     App::new()
-        .add_systems(Startup, (spawn_basic_scene, spawn_camera, spawn_light))
-        // .add_systems(Startup, spawn_gltf)
         .add_plugins(DefaultPlugins)
         .add_plugins(WorldInspectorPlugin::new())
+        .register_type::<Tower>()
+        .add_systems(Startup, (spawn_basic_scene, spawn_camera, spawn_light, asset_loading))
+        .add_systems(Update, tower_shooting)
+        .add_systems(Update, bullet_despawn)
         .run();
 }
 
 fn spawn_light(mut commands: Commands) {
     let light = PointLightBundle {
         point_light: PointLight {
+            intensity: 2_500_000.0,
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        transform: Transform::from_xyz(3.0, 8.0, 3.0),
         ..default()
     };
     commands.spawn(light).insert(Name::new("Light"));
@@ -29,6 +34,17 @@ fn spawn_camera(mut commands: Commands) {
             ..default()
         })
         .insert(Name::new("Camera"));
+}
+
+#[derive(Component, Reflect)]
+pub struct Tower {
+    shooting_timer: Timer,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Lifetime {
+    timer: Timer,
 }
 
 fn spawn_basic_scene(
@@ -46,25 +62,84 @@ fn spawn_basic_scene(
 
     commands
         .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(Cuboid::new(0.5, 0.5, 0.5))),
-            material: materials.add(Color::rgb(0.67, 0.84, 0.92)),
+            mesh: meshes.add(Mesh::from(Cuboid::new(0.5, 1.0, 0.5))),
+            material: materials.add(Color::GRAY),
             transform: Transform::from_xyz(0.0, 0.25, 0.0),
             ..default()
         })
-        .insert(Name::new("Cube"));
+        .insert(Tower {
+            shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+        })
+        .insert(Name::new("Tower"));
 }
+
+fn tower_shooting(
+    mut commands: Commands,
+    mut towers: Query<&mut Tower>,
+    bullet_assets: Res<GameAssets>,
+    time: Res<Time>,
+) {
+    for mut tower in &mut towers {
+        tower.shooting_timer.tick(time.delta());
+        if tower.shooting_timer.just_finished() {
+            let spawn_transform =
+                Transform::from_xyz(0.0, 0.7, 0.6).with_rotation(Quat::from_rotation_y(-PI / 2.0));
+
+            commands
+                .spawn(SceneBundle{
+                    scene: bullet_assets.bullet_scene.clone(),
+                    transform: spawn_transform,
+                    ..Default::default()
+                })
+                .insert(Lifetime {
+                    timer: Timer::from_seconds(0.5, TimerMode::Once),
+                })
+                .insert(Name::new("Bullet"));
+        }
+    }
+}
+
+fn bullet_despawn(
+    mut commands: Commands,
+    mut bullets: Query<(Entity, &mut Lifetime)>,
+    time: Res<Time>,
+) {
+    for (entity, mut lifetime) in &mut bullets {
+        lifetime.timer.tick(time.delta());
+        if lifetime.timer.just_finished(){
+            // always use despawn_recursive, since it will despawn all children of the entity
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct GameAssets {
+    bullet_scene: Handle<Scene>
+}
+
+fn asset_loading(mut commands: Commands, assets: Res<AssetServer>){
+    let bullet: Handle<Scene> = assets.load("Bullet.glb#Scene0");
+    commands.insert_resource(GameAssets{ bullet_scene: bullet });
+}
+
+
+
+// #[derive(Component, Reflect)]
+// pub struct Ferris;
 
 // fn spawn_gltf(mut commands: Commands, asset_server: Res<AssetServer>) {
 //     // note that we have to include the `Scene0` label
-//     let my_gltf = asset_server.load("Ferris.glb#Scene0");
+//     let my_gltf = asset_server.load("ferris3d.glb#Scene0");
 
 //     // to position our 3d model, simply use the Transform
 //     // in the SceneBundle
 //     commands
 //         .spawn(SceneBundle {
 //             scene: my_gltf,
-//             transform: Transform::from_xyz(0.0, 0.0, 0.0),
+//             transform: Transform::from_xyz(1.0, 0.0, 0.0),
 //             ..Default::default()
 //         })
+//         .insert(Ferris)
 //         .insert(Name::new("Ferris"));
 // }
